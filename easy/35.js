5564ec43a3be1eb89dbb5137c5222179e00a1e88
INTRA\quang.pham <quang.pham@rakuten.com>
2023-06-29 16:06:36 +0700

    35

--- easy/35.js
+++ easy/35.js
@@ -6,6 +6,32 @@
 var searchInsert = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
+
+  while (left <= right) {
+    let middle = parseInt((left + right) / 2);
+
+    if (nums[middle] === target) {
+      return middle;
+    }
+
+    if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+
+  return left;
 };
 
-searchInsert([1, 3, 5, 6], 7);
+console.log(searchInsert([1, 3, 5, 6], 2));
+
+// 0 3
+
+// middle = 2 => true : 1
+
+// 1 3
+//middle = 2 => true: 2 3
+
+// 2 3
+// middle = 2 =>
